33c28b6ba4c2a38acf6a56f0011c68754da94674
Cheryl Dee <cdmccalop@gmail.com>
2023-03-12 01:19:11 -0800

    lab 06 done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-
+import './Map.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,6 +11,7 @@ class App extends React.Component {
       city: '',
       cityName: '',
       cityData: [],
+      cityMap: '',
       error: false,
       errorMessage: ''
     }
@@ -30,16 +31,17 @@ getCityData = async (event) => {
     let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json&limit=1`
 
     let cityDataFromAxios = await axios.get(url)
-    console.log(cityDataFromAxios.data)
+
+    let lat = this.state.cityData.lat;
+    let lon = this.state.cityData.lon;
     
     this.setState({
       cityData: cityDataFromAxios.data[0],
       error: false,
-      cityMap: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&markers=icon:tiny-red-cutout|${cityDataFromAxios.data[0].lat},${cityDataFromAxios.data[0].lon}&zoom=12`
+      cityMap: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${lat},${lon}&zoom=10`
     })
 
   } catch (error) {
-    console.log(error);
     this.setState({
       error: true,
       errorMessage: error.message
@@ -48,17 +50,36 @@ getCityData = async (event) => {
 }
 
 render() {
+  let lat = this.state.cityData.lat;
+  let lon = this.state.cityData.lon;
+  let cityMap = this.state.cityMap;
+  
    return (
     <>
+    <main>
+
       <h1>City Explorer</h1>
-      <main>
+
         <form onSubmit={this.getCityData}>
           <label>Enter a City<input type="text" onInput={this.handleInput}></input>
           <button type='submit'>Explore!</button>
           </label>
         </form>
-      </main>
+
+        {/*Ternary for display*/}
+
+        {
+          this.state.error
+          ? <p>{this.state.errorMessage}</p>
+          : <p>{this.state.cityData.display_name}</p>
+        }
+
+        <p>Latitude: {lat} - Longitude: {lon}</p>
+        <img src={this.state.cityMap} alt={this.state.cityName}/>
+
+        </main>
     </>
+    
    )
 }
 
